7fa30a93f2b1e0d95e2fd5044e09f70cd0ef0b6c
91jack <2764132626@qq.com>
2018-08-28 18:33:58 +0800

    1
    1

--- static/js/index.js
+++ static/js/index.js
@@ -1,25 +1,4 @@
-// 顶部导航切换效果
-			$('.header .nav a').hover(function() {
-				$(this).addClass('active').siblings().removeClass('active');
-				var imgUrl = $(this).children('img').attr('src');
-				var newImgUrl = imgUrl.replace(/1/, "2");
-				$(this).children('img').attr('src', newImgUrl);
-			}, function() {
-				var imgUrl = $(this).children('img').attr('src');
-				var newImgUrl = imgUrl.replace(/2/, "1");
-				$(this).children('img').attr('src', newImgUrl);
-			})
 
-			// 顶部导航显示隐藏	
-			var on = true;
-			$('.top-title h1').on('click', function() {
-				if(on) {
-					$('.header').hide();
-				} else {
-					$('.header').show();
-				}
-				on = !on;
-			})
 			// 地图模块
 			// 地图初始化
 			var map = new BMap.Map("map"); // 创建Map实例
